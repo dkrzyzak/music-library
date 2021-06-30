4f9ff401c4a455a528868dabb87cb8469fa34f25
apocalypseDreamer <damntheguitars@gmail.com>
2021-06-30 11:45:25 +0200

    added copy constr and comments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
+#include "main.h"
+
 #include <exception>
 #include <iostream>
 #include <string>
 #include <vector>
-#include "main.h"
 
 using namespace std;
 
+// wyjątki
 struct YearException : public exception {
     const string message() const throw() {
         return "Nie można wydać. Data wydania nie została jeszcze osiągnieta.";
@@ -23,28 +25,48 @@ class Song {
         Name = name;
     }
 
+    // konstruktor kopiujący
+    Song(const Song &s1) {
+        Artist = s1.Artist;
+        Name = s1.Name;
+    }
+
     string GetSongName() {
         return Name;
+    }
+
+    void SetSongName(string name) {
+        Name = name;
     }
 
     string GetArtist() {
         return Artist;
     }
+
+    void SetArtist(string artist) {
+        Artist = artist;
+    }
 };
 
+// klasa abstrakcyjna
 class AbstractAlbum {
+    // czysto wirtualne metody
     virtual void Release() = 0;
+    virtual void PrintTracklist() = 0;
 };
 
+// dziedziczenie
 class Album : AbstractAlbum {
    private:
     string Name;
     string Artist;
     int ReleaseYear;
     bool Released;
+    // wektory
     vector<Song> Tracklist;
 
    public:
+    // konstruktor domyślny
     Album() {}
 
     Album(string name, string artist, int year) {
@@ -54,10 +76,12 @@ class Album : AbstractAlbum {
         Released = false;
     }
 
+    // destruktor
     ~Album() {
         cout << "Usunięto z pamięci album " << GetFullName() << endl;
     }
 
+    // przeciążenie operatora +
     void operator+(Song &s) {
         AddSong(s);
     }
@@ -101,7 +125,13 @@ class Album : AbstractAlbum {
     }
 
     void PrintTracklist() {
+        // iteratory
         vector<Song>::iterator itr;
+
+        // pętla zakresowa
+        // for (auto s : Tracklist) {
+        //     cout << s.GetSongName() << endl;
+        // }
 
         cout << "\nTracklista albumu " << GetFullName() << ": " << endl;
 
@@ -132,13 +162,16 @@ int main() {
     Album a1 = Album("Science Fiction", "Brand New", 2017);
     Song s1 = Song("Brand New", "Lit Me Up");
     Song s2 = Song("Brand New", "Cant Get It Out");
+    Song s3 = s2;
+    s3.SetSongName("Waste");
     a1.AddSong(s1);
     a1 + s2;
+    a1 + s3;
     a1.PrintTracklist();
     a1.Release();
 
-    Album a2 = Album("Dobry album", "lil bażant", 2024);
-    Song sa2 = Song("ktoś inny", "jakiś tytuł");
+    Album a2 = Album("Dobry album", "Wykonawca 1", 2024);
+    Song sa2 = Song("Wykonawca 2", "jakiś tytuł");
     a2.AddSong(sa2);
 
     try {
